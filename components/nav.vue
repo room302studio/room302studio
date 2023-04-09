b7890c945a3132b7e6647a5e042b5ebf3e70eba9
EJ Fox <ejfox@ejfox.com>
2023-04-09 12:53:16 -0400

    feat: add member page, headshots, restructure app

---
+++ components/nav.vue
@@ -0,0 +1,16 @@
+<template>
+<ul class="list ma0 pv1 pv4-l ph2 ph5-l f2 tc sans-serif fw8">
+  <li class="pa1 lh-copy dib mr2 mr4-l mv0">
+    <NuxtLink class="link black underline dim" to="/">Home</NuxtLink>    
+  </li>
+  <!-- <li class="pa1 lh-copy dib mr2 mr4-l mv0">
+    <NuxtLink class="link black underline dim" to="/our-work">Our Work</NuxtLink>
+  </li> -->
+  <li class="pa1 lh-copy dib mr2 mr4-l mv0">
+    <NuxtLink class="link black underline dim" to="/members">Members</NuxtLink>
+  </li>
+</ul>
+</template>
+<script setup>
+
+</script>
