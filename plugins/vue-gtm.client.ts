73087f2144617bc374dff47b6f1b1dcfa81d219b
EJ Fox <ejfox@ejfox.com>
2023-07-02 22:06:36 -0400

    fix: tweak google ID one final time

--- plugins/vue-gtm.client.ts
+++ plugins/vue-gtm.client.ts
@@ -2,7 +2,7 @@ import { createGtm } from "@gtm-support/vue-gtm"
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(createGtm({
-    id: 'GTM-6XGQNYZQMC',
+    id: 'G-6XGQNYZQMC',
     defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
     compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`
     enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
