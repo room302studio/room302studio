a900c60f8b569934693e94e4aa5fffeffd605474
EJ Fox <ejfox@ejfox.com>
2023-08-22 09:39:27 -0400

    feat: scaffold a page that shows off our prior work

--- pages/our-work.vue
+++ pages/our-work.vue
@@ -1,6 +1,35 @@
 <template>
-<div class="white pa4">
+<div class="pa4">
   <h2>Our work</h2>
+
+  <div class="mv4 pa4 ba">
+    <h4>Ukraine Grain</h4>
+    <p>
+      https://stories.theplotline.org/ukr-grain/
+    </p>
+    <img src="" alt="">
+  </div>
+
+  <div class="mv4 pa4 ba">
+    <h4>The Margin</h4>
+    <p>
+      https://themargin.us/
+    </p>
+    <img src="" alt="">
+  </div>
+
+  <div class="mv4 pa4 ba">
+    <h4>UNHCR Streamgraph Explorer</h4>
+
+    <p>
+
+      https://www.informationisbeautifulawards.com/showcase/3206-unhcr-streamgraph-explorer
+    </p>
+    <img src="" alt="">
+  </div>
+
+
+
 </div>
 </template>
 <script setup>
