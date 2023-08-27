8a0d9b1ddfe562dad7d738dbdb5802336010cae2
EJ Fox <ejfox@ejfox.com>
2023-08-27 17:47:03 -0400

    copy: add some coach artie-driven copy for each piece

--- pages/our-work.vue
+++ pages/our-work.vue
@@ -1,37 +1,65 @@
 <template>
-<div class="pa4">
-  <h2>Our work</h2>
+  <div class="pa4">
+    <h2>Our work</h2>
 
-  <div class="mv4 pa4 ba">
-    <h4>Ukraine Grain</h4>
-    <p>
-      https://stories.theplotline.org/ukr-grain/
-    </p>
-    <img src="" alt="">
+    <div class="mv4 pa4 ba">
+      <h4>Ukraine Grain</h4>
+      <h5>Client: The Plotline</h5>
+      <p>
+        <a href="https://stories.theplotline.org/ukr-grain/">https://stories.theplotline.org/ukr-grain/</a>
+      </p>
+      <p>
+        We designed an interactive tool that uses government and COMTRADE data to model the potential impact of war in
+        Ukraine on global grain supplies. Using sophisticated data visualization techniques, we mapped out what a %
+        reduction in output would mean for Ukraine's total grain production and its downstream consumers. The result was
+        an engaging, scroll-based app that made complex data accessible and intuitive.
+      </p>
+      <img src="" alt="">
+    </div>
+
+    <div class="mv4 pa4 ba">
+      <h4>Mapping Environmental Justice</h4>
+      <h5>Client: The Margin</h5>
+      <p>
+        <a href="https://themargin.us/">https://themargin.us/</a>
+      </p>
+      <p>
+        In a series examining environmental justice's impact on different communities, we created interactive
+        scroll-powered maps. Our maps innovatively combined historical redlining maps with satellite imagery for web use.
+        The visualizations served as compelling narratives illustrating how environmental justice dynamics affect people's
+        lives.
+      </p>
+      <img src="" alt="">
+    </div>
+
+    <div class="mv4 pa4 ba">
+      <h4>UNHCR Refugee Data Visualization</h4>
+      <h5>Client: United Nations Refugee Agency</h5>
+      <p>
+        <a href="https://www.informationisbeautifulawards.com/showcase/3206-unhcr-streamgraph-explorer">https://www.informationisbeautifulawards.com/showcase/3206-unhcr-streamgraph-explorer</a>
+      </p>
+      <p>
+        This award-winning project involved creating an interactive StreamGraph for UNHCR, demonstrating refugee flows
+        over time. The project allowed viewers to navigate through rich datasets by isolating each origin or destination,
+        providing an immersive and enlightening exploration of refugee movements.
+      </p>
+      <img src="" alt="">
+    </div>
+
+    <div class="mv4 pa4 ba">
+      <h4>Under Wraps: Unseen Innovation and Impact</h4>
+      <!-- <h5>Client: United Nations Refugee Agency</h5> -->
+      <!-- Because the clients are under NDA, we are gonna do a little joke and have the client name kind of glitch out -->
+      <h5>Client: c̶o̸n̶f̷i̶d̵e̶n̸t̷i̶a̴l̴ </h5>
+
+      <p>
+        At Room 302 Studio, we're equipped to handle projects of any scale or sector, harnessing our innovative mindset to drive growth. We’ve had our expertise tapped by multinational corporations and guided start-ups towards a successful funding stage, all while ensuring complete confidentiality and respect for our NDAs.  Our work spans across sectors, from data-driven scientific analyses to AI-powered tools and interactive education.
+
+      </p>
   </div>
 
-  <div class="mv4 pa4 ba">
-    <h4>The Margin</h4>
-    <p>
-      https://themargin.us/
-    </p>
-    <img src="" alt="">
-  </div>
 
-  <div class="mv4 pa4 ba">
-    <h4>UNHCR Streamgraph Explorer</h4>
-
-    <p>
-
-      https://www.informationisbeautifulawards.com/showcase/3206-unhcr-streamgraph-explorer
-    </p>
-    <img src="" alt="">
-  </div>
-
-
-
-</div>
-</template>
+</div></template>
 <script setup>
 
 </script>
