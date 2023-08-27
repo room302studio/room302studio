7c8a1c980c51495b33685813d65eb076725f7a5a
EJ Fox <ejfox@ejfox.com>
2023-08-27 18:25:34 -0400

    feat: add our work with images

--- pages/our-work.vue
+++ pages/our-work.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="pa4">
-    <h2>Our work</h2>
+    <h1 class="f-headline mv0 pv1 serif">Our work</h1>
+
+    <div class="mv4 pa4 ba">
+      <h4>Mapping Environmental Justice</h4>
+      <h5>Client: The Margin</h5>
+      <p>
+        <a href="https://themargin.us/">https://themargin.us/</a>
+      </p>
+      <p>
+        In a series examining environmental justice's impact on different communities, we created interactive
+        scroll-powered maps and visualizations for <strong>The Margin's</strong> five inaugural stories.</p>
+        
+        <p>Our visualizations included timelines, historical redlining maps, and a scroll-powered tour of the Skagit River.
+        The visualizations served to enhance the compelling narratives illustrating how environmental justice dynamics affect people's
+        lives.
+      </p>
+      <img src="our-work-images/themargin.png" alt="">
+    </div>
+
+    <div class="mv4 pa4 ba">
+      <h4>UNHCR Refugee Data Visualization</h4>
+      <h5>Client: United Nations Refugee Agency</h5>
+      <p>
+        <a
+          href="https://www.informationisbeautifulawards.com/showcase/3206-unhcr-streamgraph-explorer">https://www.informationisbeautifulawards.com/showcase/3206-unhcr-streamgraph-explorer</a>
+      </p>
+      <p>
+        This award-winning project involved creating an interactive StreamGraph for UNHCR, demonstrating refugee flows
+        over time. The project allowed viewers to navigate through rich datasets by isolating each origin or destination,
+        providing an immersive and enlightening exploration of refugee movements with an innovative visualization.
+      </p>
+      <img src="our-work-images/unhcr.png" alt="">
+    </div>
 
     <div class="mv4 pa4 ba">
       <h4>Ukraine Grain</h4>
@@ -14,36 +46,7 @@
         reduction in output would mean for Ukraine's total grain production and its downstream consumers. The result was
         an engaging, scroll-based app that made complex data accessible and intuitive.
       </p>
-      <img src="" alt="">
-    </div>
-
-    <div class="mv4 pa4 ba">
-      <h4>Mapping Environmental Justice</h4>
-      <h5>Client: The Margin</h5>
-      <p>
-        <a href="https://themargin.us/">https://themargin.us/</a>
-      </p>
-      <p>
-        In a series examining environmental justice's impact on different communities, we created interactive
-        scroll-powered maps. Our maps innovatively combined historical redlining maps with satellite imagery for web use.
-        The visualizations served as compelling narratives illustrating how environmental justice dynamics affect people's
-        lives.
-      </p>
-      <img src="" alt="">
-    </div>
-
-    <div class="mv4 pa4 ba">
-      <h4>UNHCR Refugee Data Visualization</h4>
-      <h5>Client: United Nations Refugee Agency</h5>
-      <p>
-        <a href="https://www.informationisbeautifulawards.com/showcase/3206-unhcr-streamgraph-explorer">https://www.informationisbeautifulawards.com/showcase/3206-unhcr-streamgraph-explorer</a>
-      </p>
-      <p>
-        This award-winning project involved creating an interactive StreamGraph for UNHCR, demonstrating refugee flows
-        over time. The project allowed viewers to navigate through rich datasets by isolating each origin or destination,
-        providing an immersive and enlightening exploration of refugee movements.
-      </p>
-      <img src="" alt="">
+      <img src="our-work-images/ukrainegrain.png" alt="">
     </div>
 
     <div class="mv4 pa4 ba">
@@ -53,13 +56,17 @@
       <h5>Client: c̶o̸n̶f̷i̶d̵e̶n̸t̷i̶a̴l̴ </h5>
 
       <p>
-        At Room 302 Studio, we're equipped to handle projects of any scale or sector, harnessing our innovative mindset to drive growth. We’ve had our expertise tapped by multinational corporations and guided start-ups towards a successful funding stage, all while ensuring complete confidentiality and respect for our NDAs.  Our work spans across sectors, from data-driven scientific analyses to AI-powered tools and interactive education.
+        At Room 302 Studio, we're equipped to handle projects of any scale or sector, harnessing our innovative mindset to
+        drive growth. We’ve had our expertise tapped by multinational corporations and guided start-ups towards a
+        successful funding stage, all while ensuring complete confidentiality and respect for our NDAs. Our work spans
+        across sectors, from data-driven scientific analyses to AI-powered tools and interactive education.
 
       </p>
+    </div>
+
+
   </div>
-
-
-</div></template>
+</template>
 <script setup>
 
 </script>
