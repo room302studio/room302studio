cb1cedababa7607eca07a9df2183d85543e6f51f
EJ Fox <ejfox@ejfox.com>
2023-08-27 19:58:55 -0400

    build: ssr false for netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: false, // for netlify deploy
     modules: [
     '@vueuse/nuxt',
     '@nuxt/image',
