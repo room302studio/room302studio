5ff937d8323c2a156283cfc355fe78d3b30e9896
EJ Fox <ejfox@ejfox.com>
2023-09-12 16:02:56 -0400

    copy: update homepage copy

--- components/MailchimpSignupForm.vue
+++ components/MailchimpSignupForm.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="">
-    <div id="mc_embed_shell" class="center pa2 pv4 sans-serif">
+    <div id="mc_embed_shell" class="center pa3 sans-serif bg-orange br4">
       <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
-      <div id="mc_embed_signup" class="center tl pv3 br2 bg-dark-blue white">
+      <div id="mc_embed_signup" class="center tl pv3 br2 bg-orange white">
         <form
           action="https://studio.us21.list-manage.com/subscribe/post?u=d293b3249ec6d06eab05818f9&amp;id=b45dde966b&amp;f_id=00e9dae6f0"
           method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self"
           novalidate="">
           <div id="mc_embed_signup_scroll">
-            <h2>Subscribe for updates on what we're working on...</h2>
+            <h2 class="lh-title">Subscribe for updates on what we're working on...</h2>
             <!-- <div class="indicates-required"><span class="asterisk">*</span> indicates required</div> -->
             <div class="mc-field-group">
               <label for="mce-EMAIL">Email Address
                 <!-- <span
                     class="asterisk">*</span> -->
               </label>
-              <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""
+              <input type="email" name="EMAIL" class="f2 br4 bn required email" id="mce-EMAIL" required="" value=""
                 placeholder="youremail@wherever.com">
-              <span id="mce-EMAIL-HELPERTEXT" class="helper_text bg-dark-blue white"></span>
+              <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
             </div>
             <div id="mce-responses" class="clear foot">
               <div class="response" id="mce-error-response" style="display: none;"></div>
@@ -30,8 +30,8 @@
             </div>
             <div class="optionalParent">
               <div class="clear foot">
-                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button bg-orange white"
-                  value="Subscribe">
+                <input type="submit" name="subscribe" id="mc-embedded-subscribe"
+                  class="center tc button bg-dark-blue white" value="Subscribe">
                 <!-- <p class="brandingLogo" style="margin: 0px auto;"><a href="http://eepurl.com/iuOCtw"
                       title="Mailchimp - email marketing made easy and fun"><img
                         src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"
@@ -45,5 +45,4 @@
 
   </div>
 </template>
-<style scoped>
-</style>
+<style scoped></style>
