72203909545e718de3ac2fa2ba7065ecc8de88be
EJ Fox <ejfox@ejfox.com>
2023-11-09 17:00:46 -0500

    add test blog posts, calendar booking, internal member page scaffolding

--- components/nav.vue
+++ components/nav.vue
@@ -1,101 +1,47 @@
 <template>
-  <ul
-    class="flex flex-col md:flex-row list py-4 lg:py-6 px-2 lg:px-5 md:text-center font-extrabold leading-none bg-black text-white">
-    <li class="inline-block p-1 mr-1 md:mr-2 lg:mr-4 my-0 md:my-1 flex-1 justify-center">
-      <NuxtLink class="link text-white underline transition-opacity duration-150 ease-in-out hover:opacity-100" to="/">
-        Home
-      </NuxtLink>
-    </li>
-    <!-- <li class="inline-block p-1 mr-1 md:mr-2 lg:mr-4 my-0">
-    <NuxtLink class="link text-white underline transition-opacity duration-150 ease-in-out hover:opacity-100" to="/our-work">Our Work</NuxtLink>
-  </li> -->
-    <li class="inline-block p-1 mr-1 md:mr-2 lg:mr-4 my-0 md:my-1 flex-1">
-      <NuxtLink class="link text-white underline transition-opacity duration-150 ease-in-out hover:opacity-100"
-        to="/members">Members
-      </NuxtLink>
-    </li>
+  <nav class="bg-white shadow dark:bg-gray-800">
+    <div class="container px-6 py-3 mx-auto md:flex md:justify-between md:items-center">
+      <div class="flex items-center justify-between">
+        <div>
+          <a class="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="#">room302.studio</a>
+        </div>
+        
+        <!-- Mobile menu button -->
+        <div class="flex md:hidden">
+          <button type="button" class="text-gray-500 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+              <path fill-rule="evenodd" d="M4 5h16a1 1 0 010 2H4a1 1 0 110-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2z"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
 
-    <li class="inline-block p-1 mr-1 md:mr-2 lg:mr-4 my-0 md:my-1 flex-1">
-      <NuxtLink class="link text-white underline transition-opacity duration-150 ease-in-out hover:opacity-100"
-        to="/our-work">
-        Our Work</NuxtLink>
-    </li>
-
-    <!-- <li class="inline-block p-1 mr-1 md:mr-2 lg:mr-4 my-0 md:my-1 flex-1">
-      <NuxtLink class="link text-white underline transition-opacity duration-150 ease-in-out hover:opacity-100"
-        to="/events">Events
-      </NuxtLink>
-    </li> -->
-
-    <li class="inline-block p-1 mr-1 md:mr-2 lg:mr-4 my-0 md:my-1 flex-1">
-      <NuxtLink class="link text-white underline transition-opacity duration-150 ease-in-out hover:opacity-100"
-        to="/blog">Blog
-      </NuxtLink>
-    </li>
-
-    <li class="inline-block p-1 mr-1 md:mr-2 lg:mr-4 my-0 md:my-1 flex-1">
-      <NuxtLink class="link text-white underline transition-opacity duration-150 ease-in-out hover:opacity-100"
-        to="/contact">Contact</NuxtLink>
-    </li>
-  </ul>
+      <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+      <div class="items-center md:flex">
+        <div class="flex flex-col md:flex-row md:mx-6">
+          <NuxtLink class="my-1 text-sm text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" to="/">Home</NuxtLink>
+          <NuxtLink class="my-1 text-sm text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" to="/members">Members</NuxtLink>
+          <NuxtLink class="my-1 text-sm text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" to="/our-work">Our Work</NuxtLink>
+          <NuxtLink class="my-1 text-sm text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" to="/blog">Blog</NuxtLink>
+          <NuxtLink class="my-1 text-sm text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" to="/contact">Contact</NuxtLink>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
-li {
-  margin-right: 8vw;
-}
-
-li a {
-  transition: opacity 1.5s cubic-bezier(0.5, 1, 0.89, 1);
-}
-
-/* make the non router-active links opacity 25% */
-li>a.router-link-exact-active {
-  opacity: 1;
-  text-decoration: underline;
-}
-
-li>a.router-link-active {
-  opacity: 1;
-}
-
-li>a {
-  opacity: 0.25;
-  text-decoration: none;
-}
-
-li>a:hover {
-  opacity: 1;
+.router-link-exact-active {
+  color: #3b82f6;
 }
 </style>
+
 <script setup>
+import { ref } from 'vue';
 
+const isOpen = ref(false);
+
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value;
+}
 </script>
-<style scoped>
-li {
-  margin-right: 8vw;
-}
-
-li a {
-  transition: opacity 1.5s cubic-bezier(0.5, 1, 0.89, 1);
-}
-
-/* make the non router-active links opacity 25% */
-li>a.router-link-exact-active {
-  opacity: 1;
-  text-decoration: underline;
-}
-
-li>a.router-link-active {
-  opacity: 1;
-}
-
-li>a {
-  opacity: 0.25;
-  text-decoration: none;
-}
-
-li>a:hover {
-  opacity: 1;
-}
-</style>
--- content/our-work/wcs.md
+++ content/our-work/wcs.md
@@ -1,1 +1,8 @@
+---
+title: "Case Study Title"
+description: "Brief description of the case study"
+date: "2022-01-01"
+image: "/path/to/feature/image.jpg"
+---
+
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac ante euismod, ultricies nunc quis, ultrices nisl. Sed euismod, nisl quis lacinia ultricies, nunc nisl ultricies nunc, quis ultricies nisl nisl vitae nisl. Sed euismod, nisl quis lacinia ultricies, nunc nisl ultricies nunc, quis ultricies nisl nisl vitae nisl. Sed euismod, nisl quis lacinia ultricies, nunc nisl ultricies nunc, quis ultricies nisl nisl vitae nisl. Sed euismod, nisl quis lacinia ultricies, nunc nisl ultricies nunc, quis ultricies nisl nisl vitae nisl.
