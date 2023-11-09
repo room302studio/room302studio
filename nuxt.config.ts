4ef85cda9db115d727e1b9f72965be679a6508ee
EJ Fox <ejfox@ejfox.com>
2023-11-09 14:49:53 -0500

    refactor: Update meta tags and app head in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,27 +28,27 @@ export default defineNuxtConfig({
     id: 'G-6XGQNYZQMC',
   },
   // css: ['tachyons/css/tachyons.min.css'],
-  // head: {
-  //   title: 'Room 302 Studio',
-  //   meta: [
-  //     { charset: 'utf-8' },
-  //     { name: 'title', content: 'Room 302 Studio' },      
-  //     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-  //     {
-  //       hid: 'description',
-  //       name: 'description',
-  //       content: 'An innovative and creative space where people collaborate on projects and cultivate ideas.'
-  //     },
-  //     { property: 'og:title', content: 'Room 302 Studio' },
-  //     { property: 'og:description', content: 'An innovative and creative space where people collaborate on projects and cultivate ideas.' },
-  //     { property: 'og:type', content: 'website' },
-  //     { property: 'og:url', content: 'https://room302.studio' },
-  //     { property: 'og:image', content: 'https://room302.studio/og-image.jpg' },
-  //     { name: 'twitter:card', content: 'summary_large_image' },
-  //     { name: 'twitter:title', content: 'Room 302 Studio' },
-  //     { name: 'twitter:description', content: 'An innovative and creative space where people collaborate on projects and cultivate ideas.' },
-  //     { name: 'twitter:image', content: 'https://room302.studio/twitter-image.jpg' }
-  //   ],
-  //   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  // }
+  app:{
+  head: {
+    title: 'Room 302 Studio',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'title', content: 'Room 302 Studio' },      
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'An innovative and creative space where people collaborate on projects and cultivate ideas.'
+      },
+      { property: 'og:title', content: 'Room 302 Studio' },
+      { property: 'og:description', content: 'An innovative and creative studio where people collaborate on projects and cultivate ideas.' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://room302.studio' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: 'Room 302 Studio' },
+      { name: 'twitter:description', content: 'An innovative and creative space where people collaborate on projects and cultivate ideas.' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  }
+}
 })
