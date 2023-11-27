337e1669dcdef9da2ba1d36fc375c1fef16c5ec3
EJ Fox <ejfox@ejfox.com>
2023-11-27 11:22:44 -0500

    fix: remove broken image links

--- pages/services.vue
+++ pages/services.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
     <UCard>
-      <img src="/path/to/hero-image.jpg" alt="Service Image" class="w-full h-64 object-cover">
+      <!-- <img src="/path/to/hero-image.jpg" alt="Service Image" class="w-full h-64 object-cover"> -->
       <h3 class="text-xl font-bold mt-4">Data Visualization</h3>
       <p class="mt-2">Description of the service and past experiences.</p>
       <a href="/path/to/past-work" class="text-blue-500 hover:underline mt-2">See our past work</a>
     </UCard>
     <UCard>
-      <img src="/path/to/hero-image.jpg" alt="Service Image" class="w-full h-64 object-cover">
+      <!-- <img src="/path/to/hero-image.jpg" alt="Service Image" class="w-full h-64 object-cover"> -->
       <h3 class="text-xl font-bold mt-4">Interactive Design</h3>
       <p class="mt-2">Description of the service and past experiences.</p>
       <a href="/path/to/past-work" class="text-blue-500 hover:underline mt-2">See our past work</a>
@@ -19,6 +19,5 @@
 <script setup>
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
 
