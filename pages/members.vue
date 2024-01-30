d8a887e803d29e8ca8dd58788331b5e44e332b6a
EJ Fox <ejfox@ejfox.com>
2024-01-30 14:49:25 -0500

    feat: add new member bio structure

--- pages/members.vue
+++ pages/members.vue
@@ -1,150 +1,11 @@
 <template>
   <div class="px-2 py-5 sm:px-5 text-gray-900 dark:text-gray-100">
-    <MemberBio
-      name="Curran Kelleher"
-      email="curran@room302.studio"
-      headshot="/member-headshots/curran-headshot.png"
-    >
-      <p>
-        Curran Kelleher is a data visualization expert and educator, committed
-        to empowering others through sharing knowledge and creating cutting-edge
-        tools. With experience at renowned organizations like Stamen and
-        McKinsey, Curran brings a wealth of expertise to the table.
-      </p>
 
-      <p>
-        Recognized in the dataviz community for his high-quality educational
-        content, Curran's passion for teaching is evident in every project he
-        undertakes. As the creator of VizHub, a groundbreaking platform for
-        creating and collaborating on D3 visualizations, he has transformed the
-        way practitioners work and learn. Currently based in India, Curran
-        continues to explore new horizons, challenging the boundaries of data
-        visualization and inspiring others along the way.
-      </p>
-    </MemberBio>
-
-    <MemberBio
-      name="EJ Fox"
-      email="ejfox@room302.studio"
-      headshot="/member-headshots/ej-headshot.png"
-    >
-      <p>
-        EJ Fox is a data visualization designer and developer with a passion for
-        creating innovative and captivating visuals. After beginning his career
-        with static infographics, EJ honed his skills and transitioned into the
-        dynamic world of coding. At NBC News, EJ helped create the modern
-        version of the Big Board which continues to be a staple in election
-        coverage to this day.
-      </p>
-
-      <p>
-        At Room 302 Studio, EJ brings a unique perspective, pushing boundaries
-        and exploring uncharted territory. Drawing inspiration from nature,
-        ecology, and art, he's dedicated to crafting experiences that leave a
-        lasting impression.
-      </p>
-    </MemberBio>
-
-    <MemberBio
-      name="Israel Gonzalez"
-      email="israel@room302.studio"
-      headshot="/member-headshots/israel-headshot.png"
-    >
-      <p>
-        Israel, a devoted software engineer, has an unwavering commitment to
-        overcoming complex challenges. From his initial coding experiences with
-        game development during his middle school years, Israel has adventured
-        through an array of technical terrains including machine learning, data
-        visualization, and mobile applications.
-      </p>
-
-      <p>
-        At Room 302 Studio, Israel showcases his innovative approach and
-        eagerness to harness the potential of AI language models. He is
-        committed to tackling diverse problems head on, from autonomous
-        problem-solving and simulations, to robotics and scientific research.
-        Inspired by his thirst for problem-solving, Israel is dedicated to
-        making a significant impact through technology.
-      </p>
-    </MemberBio>
-
-    <MemberBio
-      name="Elena Haliczer"
-      email="elena@room302.studio"
-      headshot="/member-headshots/elena-headshot.png"
-    >
-      <p>TK</p>
-
-      <p>TK</p>
-    </MemberBio>
-
-    <MemberBio
-      name="Sam Vogt"
-      email="sam@room302.studio"
-      headshot="/member-headshots/sam-headshot.png"
-    >
-      <p>
-        Sam Vogt is an experienced visual designer with a focus on meaningful
-        data narratives and visualization. Sam worked his way into visualization
-        projects starting as an illustrator and brand designer, learning to
-        encode organizational values in the elements of visual communication.
-      </p>
-
-      <p>
-        From there it was a surprisingly short jump to the world of data
-        visualization because, after all, both disciplines share foundational
-        principles such as integrity, accessibility, and emotional resonance -
-        and both strive to turn information into action.
-      </p>
-    </MemberBio>
-
-    <h3
-      class="my-0 py-0 font-bold tracking-widest dark:text-gray-200 text-4xl text-center"
-    >
-      On sabbatical:
-    </h3>
-
-    <!-- Ian Johnson -->
-    <!-- <div class="member my-5 opacity-20">
-      <img src="/member-headshots/ian-headshot.png" alt="Ian" class="h-16 rounded-md" />
-      <h3 class="text-specialorange-500 my-0 py-2 text-lg font-bold">Ian Johnson</h3>
-      <div class="member-bio prose">
-        <p>Ian Johnson is a data alchemist focused on building web-based data
-          interfaces. Ian studied scientific computing in school and learned design
-          on the job, bringing the two together to tackle sophisticated data
-          projects.</p>
-
-        <p>Ian is passionate about creating and learning in the open by
-          organizing workshops, meetups and developing open source tools like
-          Blockbuilder, a tool that has enhanced the prototyping and learning
-          experience for thousands of data visualization practitioners. Ian uses his
-          bottomless supply of curiosity combined with a strong desire to share his
-          learnings to enrich everyone who crosses his path.</p>
-      </div>
-    </div> -->
-
-    <MemberBio
-      name="Ian Johnson"
-      email="ian@room302.studio"
-      headshot="/member-headshots/ian-headshot.png"
-      class="opacity-20"
-    >
-      <p>
-        Ian Johnson is a data alchemist focused on building web-based data
-        interfaces. Ian studied scientific computing in school and learned
-        design on the job, bringing the two together to tackle sophisticated
-        data projects.
-      </p>
-
-      <p>
-        Ian is passionate about creating and learning in the open by organizing
-        workshops, meetups and developing open source tools like Blockbuilder, a
-        tool that has enhanced the prototyping and learning experience for
-        thousands of data visualization practitioners. Ian uses his bottomless
-        supply of curiosity combined with a strong desire to share his learnings
-        to enrich everyone who crosses his path.
-      </p>
-    </MemberBio>
+    <div v-for="member in members" :key="member._id" class="mb-8">
+      <MemberBio :name="member.name" :email="member.email" :headshot="member.photo">
+        <ContentRenderer :value="member" />
+      </MemberBio>
+    </div>
 
     <hr class="my-5 border-gray-300 dark:border-gray-700" />
 
@@ -156,11 +17,8 @@
       </p>
 
       <ul class="list-none pl-0">
-        <li
-          v-for="collaborator in sortedCollaborators"
-          :key="collaborator"
-          class="inline-block py-2 px-5 text-gray-500 bg-white dark:bg-gray-800 border border-gray-100 dark:border-gray-700 rounded-md m-2 font-display tracking-widest text-3xl"
-        >
+        <li v-for="collaborator in sortedCollaborators" :key="collaborator"
+          class="inline-block py-2 px-5 text-gray-500 bg-white dark:bg-gray-800 border border-gray-100 dark:border-gray-700 rounded-md m-2 font-display tracking-widest text-3xl">
           {{ collaborator }}
         </li>
       </ul>
@@ -181,5 +39,9 @@ const collaborators = [
 // make a computed to sort collaborators in alphabetical order
 
 const sortedCollaborators = collaborators.sort();
+
+const { data: members } = await useAsyncData("members", () =>
+  queryContent("members").find(),
+);
 </script>
 <style></style>
