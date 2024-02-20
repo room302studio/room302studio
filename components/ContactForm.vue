2b54887bb82f09226cb0bcef727644d6c93390cb
EJ Fox <ejfox@ejfox.com>
2024-02-20 11:19:36 -0500

    feat: add hubspot contact form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <div id="embedded-form" />
+    <div class="bg-white my-16 p-2 md:p-4 md:px-8 rounded" id="embedded-form" />
   </div>
 </template>
 
 <script setup>
 onMounted(() => {
   window.hbspt.forms.create({
-    region: "embedded-form",
+    // region: "embedded-form",
+    target: '#embedded-form',
     portalId: "44885066",
     formId: "ca331e10-7da4-4d24-a6d6-7036166c0f28"
   });
