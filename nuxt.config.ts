c3c3d48e3a70d0d47250cfa11bb69c0c6ba90d43
EJ Fox <ejfox@ejfox.com>
2024-02-20 11:29:04 -0500

    build: remove ssr false and devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {
-    enabled: true,
-  },
-  ssr: false,
+  // devtools: {
+  //   enabled: true,
+  // },
+  // ssr: false,
   nitro: {
     prerender: {
       crawlLinks: true,
